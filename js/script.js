9509253c0392cb3a96ae9953b8e70332845991c6
miuoshv <miuoshv@gmail.com>
2022-11-05 19:36:03 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('.btn2')
+const square = document.querySelector('.color')
+const btnRemoveColor = document.querySelector('.remove-color')
+
+function redColor() {
+	square.classList.add('red')
+	square.classList.remove('blue')
+	// square.classList.toggle('red')
+}
+
+function blueColor() {
+	square.classList.add('blue')
+	square.classList.remove('red')
+	// square.classList.toggle('blue')
+}
+
+function removeColor() {
+	square.classList.remove('red')
+	square.classList.remove('blue')
+}
+
+btn1.addEventListener('click', redColor)
+
+btn2.addEventListener('click', blueColor)
+
+btnRemoveColor.addEventListener('click', removeColor)
